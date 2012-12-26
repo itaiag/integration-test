f9fc44143436b955e65c39370d3ca3b0940f5840
unknown <gouria@ISXXFRANKNL1C.corp.emc.com>
2012-12-26 18:21:44 +0200

    New implementation for the JMS

--- src/main/java/org/jsystemtest/systemModule/jms/JmsSystemModule.java
+++ src/main/java/org/jsystemtest/systemModule/jms/JmsSystemModule.java
@@ -1,52 +1,49 @@
-/*
- */
 package org.jsystemtest.systemModule.jms;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
-import javax.jms.Connection;
-import javax.jms.DeliveryMode;
-import javax.jms.ExceptionListener;
+import javax.annotation.PostConstruct;
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageListener;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
 import javax.jms.QueueBrowser;
+import javax.jms.QueueConnectionFactory;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 
-import org.jsystemtest.systemModule.AbstractSystemModule;
-import org.springframework.jms.connection.SingleConnectionFactory;
+import org.springframework.jms.core.BrowserCallback;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.jms.support.converter.MessageType;
+import org.testng.Reporter;
 
 /**
  * <b>Package:</b> com.rsa.fa.blackbox.integration.jms<br/>
  * <b>Type:</b> Jms<br/>
- * <b>Description:</b>The module responsible for interacting with JMS. It exposes various services for sending and asserting
- * messages <br/>
+ * <b>Description:</b>The module responsible for interacting with JMS. It
+ * exposes various services for sending and asserting messages <br/>
  * 
  */
-public class JmsSystemModule extends AbstractSystemModule {
+public class JmsSystemModule {
 
-	private final SingleConnectionFactory connectionFactory;
-	private boolean transacted = false;
-
-	private List<Message> messageList = new ArrayList<Message>();
-	private final List<JMSException> exceptionList = new ArrayList<JMSException>();
-	private Map<String, Connection> connectionMap = new TreeMap<String, Connection>();
-	private CollectorConsumer consumer = new CollectorConsumer();
+	private final QueueConnectionFactory connectionFactory;
+	private JmsTemplate jmsTemplate;
 
 	/**
 	 * @param connectionFactory
 	 */
-	public JmsSystemModule(SingleConnectionFactory connectionFactory) {
+	public JmsSystemModule(QueueConnectionFactory connectionFactory) {
 		super();
 		this.connectionFactory = connectionFactory;
+	}
+
+	@PostConstruct
+	public void createConnection() throws JMSException {
+		Reporter.log("Creating JMS connection", true);
+		jmsTemplate = new JmsTemplate(connectionFactory);
+
 	}
 
 	/**
@@ -56,34 +53,87 @@ public class JmsSystemModule extends AbstractSystemModule {
 	 *            The name of the destination queue
 	 * @param type
 	 *            TEXT,MAP,BYTES or OBJECT
-	 * @param messageBody
+	 * @param messageObject
 	 * @throws Exception
-	 *             if queue is not exist of failed to create connection to queue or to close the queue
+	 *             if queue is not exist of failed to create connection to queue
+	 *             or to close the queue
 	 * @throws IllegalArgumentException
 	 *             If message type is not supported
 	 */
-	public void sendMessage(String queueName, MessageType type, String messageBody) throws Exception {
-		reporter.report("About to send message from type " + type.name() + " to queue " + queueName);
-		Connection connection = connectionFactory.createConnection();
-		try {
-			connection.start();
-			Session session = connection.createSession(transacted, Session.AUTO_ACKNOWLEDGE);
-			Queue queue = session.createQueue(queueName);
-			Message message = null;
-			switch (type) {
+	public void sendMessage(final String queueName, final MessageType type, final Object messageObject) throws Exception {
+		Reporter.log("Sending message from type " + type.name() + " to queue " + queueName, true);
+		jmsTemplate.send(queueName, new MessageCreator() {
+			public Message createMessage(Session session) throws JMSException {
+				switch (type) {
 				case TEXT:
-					message = session.createTextMessage(messageBody);
+					if (messageObject instanceof String) {
+						return session.createTextMessage((String) messageObject);
+					} else {
+						throw new IllegalArgumentException("Cannot send as text message object is not String " + type.name());
+					}
+				case OBJECT:
+					if (messageObject instanceof Serializable)
+						return session.createObjectMessage((Serializable) messageObject);
 					break;
-
 				default:
 					throw new IllegalArgumentException("Unsupported type " + type.name());
-			}
-			MessageProducer producer = session.createProducer(queue);
-			producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-			producer.send(message);
-		} finally {
-			connection.close();
+				}
+				return null;
+
+			};
+		});
+
+	}
+
+	/**
+	 * Receive one message from queue
+	 * 
+	 * @param queueName
+	 * @param timeout
+	 *            time to wait in millis for message. specify zero if no timeout
+	 *            is required
+	 * @return message from queue or null if none exist
+	 * @throws JMSException
+	 */
+	public Message receiveMessage(final String queueName, long timeout) throws JMSException {
+		if (timeout == 0) {
+			timeout = 1;
 		}
+		Reporter.log("Receiving message from queue " + queueName, true);
+		jmsTemplate.setReceiveTimeout(timeout);
+		Message message = jmsTemplate.receive(queueName);
+		if (null != message) {
+			StringBuilder messageReport = new StringBuilder();
+			messageReport.append("Message from type ").append(message.getClass().getSimpleName()).append(" was received");
+			if (message instanceof TextMessage) {
+				messageReport.append(" with content '");
+				try {
+					messageReport.append(((TextMessage) message).getText()).append("'");
+				} catch (JMSException e) {
+					messageReport.append("<Failed getting message content>");
+				}
+			}
+			Reporter.log(messageReport.toString(), true);
+
+		}
+		return message;
+
+	}
+
+	/**
+	 * Receive all the messages in a given queue.
+	 * 
+	 * @param queueName
+	 * @return list of messages. If none exist in queue the list would be empty
+	 * @throws JMSException
+	 */
+	public List<Message> recieveAllMessages(final String queueName) throws JMSException {
+		final List<Message> messages = new ArrayList<Message>();
+		Message message = null;
+		while ((message = receiveMessage(queueName, 0)) != null) {
+			messages.add(message);
+		}
+		return messages;
 	}
 
 	/**
@@ -95,116 +145,32 @@ public class JmsSystemModule extends AbstractSystemModule {
 	 *             If queue is not exist or failed to create connection
 	 */
 	public List<Message> browseQueue(String queueName) throws Exception {
-		Connection connection = null;
-		connection = connectionFactory.createConnection();
-		List<Message> messageList = null;
-		try {
-			connection.start();
-			Session session = connection.createSession(transacted, Session.AUTO_ACKNOWLEDGE);
-			Queue queue = session.createQueue(queueName);
-			QueueBrowser browser = session.createBrowser(queue);
-			messageList = new ArrayList<Message>();
-			@SuppressWarnings("unchecked")
-			Enumeration<Message> e = browser.getEnumeration();
-			while (e.hasMoreElements()) {
-				messageList.add(e.nextElement());
+		Reporter.log("Browsing queue " + queueName, true);
+		final List<Message> messages = jmsTemplate.browse("Blocking", new BrowserCallback<List<Message>>() {
+			@Override
+			public List<Message> doInJms(Session session, QueueBrowser browser) throws JMSException {
+				List<Message> messages = new ArrayList<Message>();
+				@SuppressWarnings("unchecked")
+				Enumeration<Message> e = browser.getEnumeration();
+				while (e.hasMoreElements()) {
+					messages.add(e.nextElement());
 
+				}
+				return messages;
 			}
-		} finally {
-			connection.stop();
-		}
-
-		return messageList;
+		});
+		return messages;
 	}
 
+	/**
+	 * Cleans all messages in queue
+	 * 
+	 * @param queueName
+	 * @throws Exception
+	 */
 	public void flushQueue(String queueName) throws Exception {
-		// TODO: Implement fulshQueue.
-	}
-
-	/**
-	 * Opens connection to JMS and start listen to the specified queue. Use getMessageList to receive the captured messages
-	 * 
-	 * @param queueName
-	 * @throws Exception
-	 *             If failed to create connection or queue is not exist
-	 */
-	public void startListenToQueue(String queueName) throws Exception {
-		Connection connection = null;
-		connection = connectionFactory.createConnection();
-		connection.start();
-		Session session = connection.createSession(transacted, Session.AUTO_ACKNOWLEDGE);
-		Queue queue = session.createQueue(queueName);
-		MessageConsumer messageConsumer = session.createConsumer(queue);
-		connection.setExceptionListener(consumer);
-		messageConsumer.setMessageListener(consumer);
-		connectionMap.put(queueName, connection);
-	}
-
-	/**
-	 * Stop listens to queue. If queue is not being listen to, nothing will happen.
-	 * 
-	 * @param queueName
-	 *            queue to stop listen to.
-	 * @throws Exception
-	 *             If failed to close connection
-	 */
-	public void stopListenToQueue(String queueName) throws Exception {
-		if (connectionMap.containsKey(queueName)) {
-			Connection connection = connectionMap.remove(queueName);
-			connection.close();
-		}
-
-	}
-
-	/**
-	 * Stops listening to all the queues.
-	 * 
-	 * @throws Exception
-	 *             If failed to stop connection.
-	 */
-	public void stopListenToQueues() throws Exception {
-		for (String queueName : connectionMap.keySet()) {
-			stopListenToQueue(queueName);
-		}
-	}
-
-	/**
-	 * Cleans all the message that has been captured so far.
-	 */
-	public void flushMessages() {
-		messageList = new ArrayList<Message>();
-	}
-
-	public List<Message> getMessageList() {
-		// TODO: Should be cloned
-		// TODO: Consider add the option to receive message of specific queue
-		return messageList;
-	}
-
-	public boolean isTransacted() {
-		return transacted;
-	}
-
-	public void setTransacted(boolean transacted) {
-		this.transacted = transacted;
-	}
-
-	/**
-	 * 
-	 * This class uses for capturing all the messages received from the queues.
-	 */
-	private class CollectorConsumer implements MessageListener, ExceptionListener {
-
-		@Override
-		public void onMessage(Message message) {
-			messageList.add(message);
-		}
-
-
-		@Override
-		public void onException(JMSException e) {
-			exceptionList.add(e);
-			
+		Reporter.log("Flusing queue " + queueName, true);
+		while (receiveMessage(queueName, 1000) != null) {
 		}
 	}
 
--- src/test/java/org/jsystemtest/unit/JmsSystemModuleTests.java
+++ src/test/java/org/jsystemtest/unit/JmsSystemModuleTests.java
@@ -1,12 +1,4 @@
-/*
- * Organization: RSA
- * Product:      Blackbox
- * Version:      1.0.1
- * Project:		 blackbox-integration-test
- */
 package org.jsystemtest.unit;
-
-import java.util.List;
 
 import javax.jms.Message;
 
@@ -17,6 +9,8 @@ import org.jsystemtest.systemModule.jms.JmsSystemModule;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.support.converter.MessageType;
+import org.testng.Reporter;
+import org.testng.annotations.BeforeMethod;
 
 /**
  * <b>Package:</b> com.rsa.fa.blackbox.integration<br/>
@@ -28,27 +22,67 @@ import org.springframework.jms.support.converter.MessageType;
 public class JmsSystemModuleTests extends AbstractIntegrationTestCase {
 
 	private static final String QUEUE_NAME = "Blocking";
+
+	private static final long MESSAGE_TIMEOUT = 10;
+
 	@Autowired
 	private JmsSystemModule jms;
 
+	@BeforeMethod
+	public void clearResources() throws Exception {
+		Reporter.log("Clearing queue before test", true);
+		jms.flushQueue(QUEUE_NAME);
+		Reporter.log("Finished clearing queue", true);
+	}
+
+	@Test
+	public void testFlushQueue() throws Exception {
+		for (int i = 0; i < 10; i++) {
+			jms.sendMessage(QUEUE_NAME, MessageType.TEXT, "testFlushQueue");
+		}
+		jms.flushQueue(QUEUE_NAME);
+		Assert.assertNull(jms.receiveMessage(QUEUE_NAME, 1));
+		Reporter.log("Success", true);
+	}
+
 	@Test
 	public void testSendRecieveMessage() throws Exception {
-		jms.sendMessage(QUEUE_NAME, MessageType.TEXT, "My message");
-		jms.startListenToQueue(QUEUE_NAME);
-		sleep(1000);
-		Assert.assertEquals(1, jms.getMessageList().size());
-		System.out.println("Success");
-		jms.stopListenToQueue(QUEUE_NAME);
+		jms.sendMessage(QUEUE_NAME, MessageType.TEXT, "testSendRecieveMessage0");
+		Message m = jms.receiveMessage(QUEUE_NAME, MESSAGE_TIMEOUT);
+		Assert.assertNotNull(m);
+		jms.sendMessage(QUEUE_NAME, MessageType.TEXT, "testSendRecieveMessage0");
+		m = jms.receiveMessage(QUEUE_NAME, MESSAGE_TIMEOUT);
+		Assert.assertNotNull(m);
+		Reporter.log("Success", true);
 	}
 
 	@Test
 	public void testBrowseQueue() throws Exception {
-		jms.sendMessage(QUEUE_NAME, MessageType.TEXT, "My message");
-		sleep(1000);
-		List<Message> message = jms.browseQueue(QUEUE_NAME);
-		Assert.assertEquals(1, message.size());
-		Assert.assertEquals(1, message.size());
-		System.out.println("Success");
+		int numOfMessages = 2;
+		for (int i = 0; i < numOfMessages; i++) {
+			jms.sendMessage(QUEUE_NAME, MessageType.TEXT, "testFlushQueue" + i);
+		}
+		Assert.assertEquals(3, jms.browseQueue(QUEUE_NAME).size());
+		Assert.assertEquals(3, jms.browseQueue(QUEUE_NAME).size());
+		
+		Reporter.log("Success", true);
+		
+		//Cleaning queue
+		for (int i = 0; i < numOfMessages; i++) {
+			jms.receiveMessage(QUEUE_NAME, 500);
+		}
+		
 	}
+	
+	@Test
+	public void testReceiveAllMessages() throws Exception{
+		for (int i = 0; i < 3; i++) {
+			jms.sendMessage(QUEUE_NAME, MessageType.TEXT, "testFlushQueue" + i);
+		}
+		sleep(1000);
+		Assert.assertEquals(3, jms.recieveAllMessages(QUEUE_NAME).size());
+		
+	}
+
 
 }
