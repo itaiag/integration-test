136854821d120b4de9a95fa911755468c57dc3ac
itaiag <itai.agmon@gmail.com>
2013-02-07 18:02:25 +0200

    Groovy support

---
+++ src/test/java/org/jsystemtest/integration/GroovyTest.groovy
@@ -0,0 +1,13 @@
+package org.jsystemtest.integration
+
+import org.testng.annotations.Test;
+import org.jsystemtest.infra.report.Reporter;
+class GroovyTest {
+
+	@Test
+	void testGroovy(){
+		
+		System.out.println("STDOUT OF GROOVY TEST");
+		Reporter.log("IN GROOVY TEST!!!");
+	}
+}
