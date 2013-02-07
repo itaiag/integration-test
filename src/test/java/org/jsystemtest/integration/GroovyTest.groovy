90f4538d3dacd2b3b0cc9a61c4d28ad5a05a19ce
itaiag <itai.agmon@gmail.com>
2013-02-07 19:22:58 +0200

    Groovy test

--- src/test/java/org/jsystemtest/integration/GroovyTest.groovy
+++ src/test/java/org/jsystemtest/integration/GroovyTest.groovy
@@ -3,11 +3,35 @@ package org.jsystemtest.integration
 import org.testng.annotations.Test;
 import org.jsystemtest.infra.report.Reporter;
 class GroovyTest {
+	
+	static class Bdd {
+		
+		void given(String str){
+			System.out.println(str);	
+		}
+		
+		static Bdd exec(Closure script){
+			def bdd = new Bdd()
+			script.delegate = bdd
+			script()
+			bdd
+		}
+	}
+	
 
 	@Test
 	void testGroovy(){
+		def bbd = {
+			given "All kind of things"
+			
+			}
 		
-		System.out.println("STDOUT OF GROOVY TEST");
-		Reporter.log("IN GROOVY TEST!!!");
+	
+	}
+	
+	def execute(Closure... closures){
+		for (Closure closure: closures){
+			closure.call()
+		}
 	}
 }
